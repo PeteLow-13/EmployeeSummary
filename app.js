2f60ed397e16cd0f0964a7c670effeb0e7da9b2d
Pete Low <pedritolow@gmail.com>
2020-08-25 20:49:48 -0700

    reorganized things to proper places

--- app.js
+++ app.js
@@ -8,10 +8,87 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+const render = require("./lib/htmlRender");
 
 
 // Write code to use inquirer to gather information about the development team members,
+
+var questions =  [{
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+    default: '',
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'What is your id?',
+    default: '',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?',
+    default: '',
+  },
+  {
+    type: 'list',
+    name: 'role',
+    message: 'What is your role?',
+    choices:['Manager', 'Engineer', 'Intern'],
+    default: 'Employee',
+  },
+];
+
+inquirer.prompt(questions).then((answer) => {
+    const{name,id,email} = answer;
+    if( answer.role = 'Engineer'){
+        inquirer.prompt ([ 
+            { 
+                type: 'input',
+                name: 'gitHub',
+                message: 'What is your Github user name?',
+                default: '',
+            },
+    ]).then((answer) => {
+        const{github} = answer;
+        let engineer = new Engineer (name,id,email,github);
+        console.log(engineer)
+    })
+
+   };
+    if(answer.role = 'Intern'){
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'school',
+                message: 'What is your school?',
+                default: '',
+            },
+        ]).then((answer => {
+            const{school} = answer;
+            let intern = new Intern (name,id,email,shcool);
+            console.log(intern)
+        }))
+    }
+    if(answer.role = 'Manager'){
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'officenumber',
+                message: 'What is your office number?',
+                default: '',
+              },
+        ]).then((answer => {
+            const{officenumber} = answer;
+            let manager = new Manager (name,id,email,officenumber);
+            console.log(manager)
+        }))
+    }
+
+    
+    console.log(answer)
+    });
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 // After the user has input all employees desired, call the `render` function (required
