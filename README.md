4ca61d293264b065f0382e8faa6ece5946ae813b
Pete Low <pedritolow@gmail.com>
2020-09-05 17:25:48 -0700

    added read me

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# EmployeeSummary
+# Employee Summary
+[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT)
+
+This can be used to generate an employee directory for different types of employees.
+## Table of Contents
+<!-- - [Installation](#installation) -->
+- [Usage](#usage)
+<!-- - [License](#license) -->
+- [Contributing](#contributing)
+- [Images](#images)
+- [Questions](#questions)
+<!-- ## Installation
+
+Installation -->
+## Usage
+
+This project utilizes inquirer to help generate employee contact info for three different types of compamny roles.
+## License
+
+License
+## Contributing
+
+Contributing
+## Images
+<iframe src="https://drive.google.com/file/d/17IPLk-RTxwtmYFP8Y50ypPQE8TbosNh0/preview" width="640" height="480"></iframe>
+Screen shot of rendered contact page in browser: 
+![Image](./assets/empsumm.png)
+
+## Repository
+- https://github.com/PeteLow-13/EmployeeSummary
+## Questions
+
+Contact me via email or github with questions
+- pedritolow@gmail.com
+- [PeteLow-13](http://github.com/github)
--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
-const render = require("./lib/htmlRender");
+const render = require('./lib/htmlRender');
 
 
 // Write code to use inquirer to gather information about the development team members,
@@ -19,6 +19,13 @@ var questions =  [{
     name: 'name',
     message: 'What is employee name?',
     default: '',
+    validate: async (input) => {
+        if (input === ''){
+            return 'Please enter valid name';
+        }
+        return true;
+    }
+    
   },
   {
     type: 'input',
@@ -31,6 +38,12 @@ var questions =  [{
     name: 'email',
     message: 'What is employee email?',
     default: '',
+    validate: async (input) => {
+        if (/^(\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+[,;]?[ ]?)+$/.test(input)){
+            return true;
+        }
+        return 'please enter a valid email address.';
+    }
   },
   //this question determines role which generates correct prompt for role specific question
   {
@@ -110,7 +123,11 @@ var addEmployee = function(){
         }
         
     });
+
+    
 }
+
+
 
 var addAnotherEmployee = function(answer){
 
@@ -119,7 +136,13 @@ var addAnotherEmployee = function(answer){
     } else {
         var renderedTeam = render(team);
         console.log(renderedTeam)
+        fs.writeFile(outputPath, renderedTeam, (err) => {
+            if (err) throw err
+            console.log('sucess')
+        });
     }
+        
+    
 }
 addEmployee();
 
